e8a681fee20844eb1e8444ecaadeee41e27f66d6
cbelloda <cbelloda@gmail.com>
2020-08-21 19:23:53 -0500

    add robustscaler dependency

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn.preprocessing import RobustScaler
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
